c267336aea9f29388f9f14a9db41d47eb27e0b2a
icarobteles <eng.icaroteles@gmail.com>
2022-06-09 20:01:41 -0300

    feat(providers): :sparkles: add AuthProvider and UserProvider in Providers component

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -1,7 +1,15 @@
+import { AuthProvider } from "./auth";
 import { ThemeManagerProvider } from "./theme";
+import { UserProvider } from "./user";
 
 const Providers = ({ children }) => {
-  return <ThemeManagerProvider>{children}</ThemeManagerProvider>;
+  return (
+    <ThemeManagerProvider>
+      <AuthProvider>
+        <UserProvider>{children}</UserProvider>
+      </AuthProvider>
+    </ThemeManagerProvider>
+  );
 };
 
 export default Providers;

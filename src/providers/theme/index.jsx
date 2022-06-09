1c976a4760d5384576fc3fd5af2fab8c5d9af736
icarobteles <eng.icaroteles@gmail.com>
2022-06-09 19:59:04 -0300

    feat(providers): :sparkles: add currentLogo state in ThemeManagerProvider

--- src/providers/theme/index.jsx
+++ src/providers/theme/index.jsx
@@ -1,21 +1,32 @@
 import { createContext, useCallback, useContext, useState } from "react";
+import logoDark from "../../assets/images/logoDark.svg";
+import logoLight from "../../assets/images/logoLight.svg";
 
 const ThemeManagerContext = createContext();
 
 export const ThemeManagerProvider = ({ children }) => {
   const [currentTheme, setCurrentTheme] = useState("dark");
+  const [currentLogo, setCurrentLogo] = useState(logoDark);
 
   const getOpositeTheme = useCallback(
     () => (currentTheme === "light" ? "dark" : "light"),
     [currentTheme]
   );
 
+  const getOpositeLogo = useCallback(
+    () => (currentTheme === "light" ? logoDark : logoLight),
+    [currentTheme]
+  );
+
   const changeTheme = () => {
     setCurrentTheme(getOpositeTheme());
+    setCurrentLogo(getOpositeLogo());
   };
 
   return (
-    <ThemeManagerContext.Provider value={{ currentTheme, changeTheme }}>
+    <ThemeManagerContext.Provider
+      value={{ currentTheme, changeTheme, currentLogo }}
+    >
       {children}
     </ThemeManagerContext.Provider>
   );

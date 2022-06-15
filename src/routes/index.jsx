d6e847891a731e382f96ef48922654049f54c6a7
icarobteles <eng.icaroteles@gmail.com>
2022-06-15 10:27:16 -0300

    refactor: :fire: remove LandingPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import { Routes as RoutesFromRouterDOM, Route } from "react-router-dom";
 
 //IMPORT PAGES
-import LandingPage from "../pages/Landing";
 import LoginPage from "../pages/Login";
 import MainPage from "../pages/Main";
 import ProfilePage from "../pages/Profile";
@@ -10,8 +9,7 @@ import RegisterPage from "../pages/Register";
 const Routes = () => {
   return (
     <RoutesFromRouterDOM>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/admin/main" element={<MainPage />} />
       <Route path="/admin/profile" element={<ProfilePage />} />
